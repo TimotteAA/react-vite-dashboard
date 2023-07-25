5df6b338513247029bafc040ce23e20a40a033e3
timotte <azurlane202018@gmail.com>
2023-07-26 07:24:06 +0800

    feat: 完成部分router模块utils函数

--- src/components/router/utils.ts
+++ src/components/router/utils.ts
@@ -91,3 +91,53 @@ export const getFlatRoutes = (routes: RouteOption[]): RouteOption[] =>
             return route.children?.length ? [route, ...getFlatRoutes(route.children)] : [route];
         })
         .reduce((o, n) => [...o, ...n], []);
+
+/**
+ * 获取带全路径的路由
+ * @param routes 原始路由数组
+ * @param parentPath 父级路径，用于递归调用
+ */
+export const getFullPathRoutes = (routes: RouteOption[], parentPath?: string): RouteOption[] =>
+    routes
+        .map((route) => {
+            // 如果当前路由被标记为 devide，则跳过处理，返回空数组
+            if (route.divide) return [];
+
+            // 深复制当前路由对象，为避免修改原对象
+            const item: RouteOption = { ...route };
+
+            // 初始化父路径和子路径前缀
+            const pathPrefix: { parent?: string; child?: string } = {
+                parent: trim(parentPath ?? '', '/').length
+                    ? `/${trim(parentPath ?? '', '/')}/`
+                    : '/',
+                child: trim(parentPath ?? '', '/').length ? `/${trim(parentPath ?? '', '/')}` : '/',
+            };
+
+            // 如果当前路由被标记为 devide 或 index，则剔除掉 children 和 path 属性，并返回
+            if (route.divide || route.index) return [omit(route, ['children', 'path'])];
+
+            // 判断路由路径是否是 URL，如果是，则直接赋值给 item.path
+            if (isUrl(route.path)) {
+                item.path = route.path;
+            } else {
+                // 如果不是 URL，拼接父路径和子路径生成全路径
+                pathPrefix.child = route.path?.length
+                    ? `${pathPrefix.parent}${trim(route.path, '/')}`
+                    : pathPrefix.child;
+                item.path = route.onlyGroup ? undefined : pathPrefix.child;
+            }
+
+            // 处理子路由，递归调用当前函数
+            item.children = route.children?.length
+                ? getFullPathRoutes(route.children, pathPrefix.child)
+                : undefined;
+
+            // 如果当前路由被标记为 onlyGroup，并且没有子路由，则将其 children 属性设置为 []
+            if (route.onlyGroup) item.children = item.children?.length ? item.children : [];
+
+            // 返回处理过的路由对象
+            return [item];
+        })
+        // 使用 reduce 将 map 返回的二维数组合并成一维数组
+        .reduce((o, n) => [...o, ...n], []);

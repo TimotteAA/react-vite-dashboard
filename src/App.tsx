08c2f8b4a1a8be0e8e76671f302b83b6a31f6f14
timotte <azurlane202018@gmail.com>
2023-07-30 19:06:07 +0800

    feat/ 添加tailwind配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { ConfigProvider } from 'antd';
+import { Button, ConfigProvider, theme } from 'antd';
+import { StyleProvider } from '@ant-design/cssinjs';
 
-import Icon from './components/icon/Icon';
-import './App.css';
+import '@/styles/index.css';
+
 import { countStore } from './store';
 
 const App = () => {
@@ -10,16 +11,23 @@ const App = () => {
     return (
         <ConfigProvider
             theme={{
+                algorithm: theme.defaultAlgorithm,
                 token: {
                     colorPrimary: '#1677ff',
                 },
             }}
         >
-            <div>
-                <h2>Test</h2>
-                <h2>count: {count}</h2>
-                <button onClick={() => increment(1)}>+1</button>
-            </div>
+            {/* 取消 :where定义的Antd样式，并解决与tailwind的样式冲突问题 */}
+            <StyleProvider hashPriority="high">
+                <div>
+                    <h2>Test</h2>
+                    <h2>count: {count}</h2>
+                    <button onClick={() => increment(1)}>+1</button>
+                </div>
+                <Button className="tw-font-standard" type="primary">
+                    tw测试
+                </Button>
+            </StyleProvider>
         </ConfigProvider>
     );
 };

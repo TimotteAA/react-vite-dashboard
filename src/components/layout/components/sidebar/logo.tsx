6c456cab8800c9e81659a9280491c303009d8a29
timotte <azurlane202018@gmail.com>
2023-08-06 17:44:03 +0800

    feat: 完成logo与样式

--- src/components/layout/components/sidebar/logo.tsx
+++ src/components/layout/components/sidebar/logo.tsx
@@ -1,5 +1,8 @@
-import { CSSProperties, FC, useMemo } from 'react';
+import { CSSProperties, FC, useCallback, useMemo } from 'react';
 import clsx from 'clsx';
+
+import { KeepAliveStore } from '@/components/keepalive/store';
+import { useNavigator } from '@/components/router/hooks';
 
 import logo from '@/assets/logo.svg';
 
@@ -9,6 +12,10 @@ import $styles from './logo.module.css';
 
 export const Logo: FC<{ style?: CSSProperties }> = ({ style }) => {
     const { collapsed } = useLayout();
+    const path = KeepAliveStore(useCallback((state) => state.path, []));
+    const navigate = useNavigator();
+
+    const goHome = useCallback(() => navigate(path), [path]);
 
     const classes = useMemo(
         () => clsx($styles.container, { logoCollapsed: collapsed }),
@@ -16,7 +23,7 @@ export const Logo: FC<{ style?: CSSProperties }> = ({ style }) => {
     );
 
     return (
-        <div style={style ?? {}} className={classes}>
+        <div style={style ?? {}} className={classes} onClick={goHome}>
             <img src={logo} alt="logo" style={{ width: '64px', height: '64px' }} />
             <span>Admin Pro</span>
         </div>
